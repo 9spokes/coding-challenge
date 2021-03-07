2bfeebb8a3c03e594bc5d56c1541e7f1bb2d6225
Bozydar Sobczak <bozysob@gmail.com>
2021-03-07 20:48:22 +1300

    Preparing test scaffold

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+group :development do
+  gem 'pry'
+  gem 'rubocop'
+end
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/evaluation_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Revenue' do
+  it 'returns proper values'
+end
+describe 'Expenses' do
+  it 'returns proper values'
+end
+describe 'Gross Profit Margin' do
+  it 'returns proper values'
+end
+describe 'Net Profit Margin' do
+  it 'returns proper values'
+end
+describe 'Working Capital Ratio' do
+  it 'returns proper values'
+end
