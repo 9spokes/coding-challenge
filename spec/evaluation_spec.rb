7070d4f4fdc0376e741f6ed85e1efc95a880a646
Bozydar Sobczak <bozysob@gmail.com>
2021-03-08 19:14:01 +1300

    Net Profit Margin and Working Capital Ratio

--- spec/evaluation_spec.rb
+++ spec/evaluation_spec.rb
@@ -35,7 +35,7 @@ describe Evaluation do
             "total_value" =>  100.0
           },
           {
-            "account_category" =>  "not-expensee",
+            "account_category" =>  "not-expense",
             "total_value" =>  666.0
           },
           {
@@ -50,12 +50,95 @@ describe Evaluation do
     end
   end
   describe 'Gross Profit Margin' do
-    it 'returns proper values'
+    let(:data) do
+      {
+        "data" =>  [
+          {
+            "account_type" =>  "sales",
+            "value_type" => "debit",
+            "total_value" =>  100.0
+          },
+          {
+            "account_type" =>  "not-salesalien",
+            "value_type" => "debit",
+            "total_value" =>  100.0
+          },
+          {
+            "account_category" =>  "revenue",
+            "total_value" =>  200.0
+          }
+        ]
+      }
+    end
+    it 'returns proper values' do
+      expect(subject.gross_profit_margin).to be == 0.5
+    end
   end
   describe 'Net Profit Margin' do
-    it 'returns proper values'
+    let(:data) do
+      {
+        "data" =>  [
+          {
+            "account_category" =>  "revenue",
+            "total_value" =>  100.0
+          },
+          {
+            "account_category" =>  "revenue",
+            "total_value" =>  200.0
+          },
+          {
+            "account_category" =>  "expense",
+            "total_value" =>  100.0
+          },
+          {
+            "account_category" =>  "expense",
+            "total_value" =>  201.0
+          }
+        ]
+      }
+    end
+    it 'returns proper values' do
+      expect(subject.net_profit_margin).to be == -1.0
+    end
   end
   describe 'Working Capital Ratio' do
-    it 'returns proper values'
+    let(:data) do
+      {
+        "data" =>  [
+          {
+            "account_category" =>  "assets",
+            "account_type" => "current",
+            "total_value" =>  300.0,
+            "value_type" => "debit"
+          },
+          {
+            "account_category" =>  "assets",
+            "account_type" => "current_accounts_receivable",
+            "total_value" =>  100.0,
+            "value_type" => "credit"
+          },
+          {
+            "account_category" =>  "liability",
+            "account_type" => "current",
+            "total_value" =>  800.0,
+            "value_type" => "debit"
+          },
+          {
+            "account_category" =>  "liability",
+            "account_type" => "current",
+            "total_value" =>  400.0,
+            "value_type" => "credit"
+          },
+          {
+            "account_category" =>  "liability",
+            "account_type" => "mistake",
+            "total_value" =>  666.0
+          }
+        ]
+      }
+    end
+    it 'returns proper values' do
+      expect(subject.working_capital_ratio).to be == 0.5
+    end
   end
 end
--- src/evaluation.rb
+++ src/evaluation.rb
@@ -1,13 +1,70 @@
 class Evaluation
+  ZERO = 0.0
+
   def initialize(data:)
     @data = data["data"]
   end
 
   def revenue
-    @data.sum { |item| item['account_category'] == 'revenue' ? item['total_value'] : 0 }
+    @revenue ||= @data.sum(ZERO) { |item| item['account_category'] == 'revenue' ? coerce(item['total_value']) : ZERO }
   end
 
   def expenses
-    @data.sum { |item| item['account_category'] == 'expense' ? item['total_value'] : 0 }
+    @expenses ||= @data.sum(ZERO) { |item| item['account_category'] == 'expense' ? coerce(item['total_value']) : ZERO }
+  end
+
+  def gross_profit_margin
+    @gross_profit_margin ||=
+      if revenue.zero?
+        :nan
+      else
+        @data.sum(ZERO) do |item|
+          item['account_type'] == 'sales' && item['value_type'] == 'debit' ? coerce(item['total_value']) : ZERO
+        end / revenue
+      end
+  end
+
+  def net_profit_margin
+    @net_profit_margin ||= revenue - expenses
+  end
+
+  def working_capital_ratio
+    @net_profit_margin ||=
+      begin
+        assets =
+          @data.sum(ZERO) do |item|
+            next ZERO unless item['account_category'] == 'assets' \
+                      && %w[current bank current_accounts_receivable].include?(item['account_type'])
+
+            credit_debit_sign(item) * coerce(item['total_value'])
+          end
+
+        liabilities =
+          @data.sum(ZERO) do |item|
+            next ZERO unless item['account_category'] == 'liability' \
+                      && %w[current current_accounts_payable].include?(item['account_type'])
+
+            credit_debit_sign(item) * coerce(item['total_value'])
+          end
+
+        liabilities.zero? ? :nan : assets / liabilities
+      end
+  end
+
+  private
+
+  def credit_debit_sign(item)
+    case item['value_type']
+    when 'debit'
+      1.0
+    when 'credit'
+      -1.0
+    else
+      ZERO
+    end
+  end
+
+  def coerce(value)
+    value || ZERO
   end
 end
