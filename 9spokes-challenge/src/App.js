400ee519e176567e290aff6e05e940dfa0cbc73c
Jordan D'Souza <jordandsouza352@gmail.com>
2021-07-29 19:12:23 +1200

    moved functions outside functional component to avoid issues with useEffect dependency array

--- 9spokes-challenge/src/App.js
+++ 9spokes-challenge/src/App.js
@@ -2,6 +2,41 @@ import './App.css';
 import {useState, useEffect} from 'react';
 import data from './data.json'
 
+  
+const getRevenue = () => {
+  var totalRevenue = 0;
+  data.data.forEach((obj)=>{
+      if(obj.account_category === 'revenue'){
+        totalRevenue = totalRevenue + obj.total_value
+      }
+  })
+  return totalRevenue;
+}
+
+const getExpenses = () => {
+  var totalExpense = 0;
+  data.data.forEach((obj)=>{
+    if(obj.account_category === 'expense'){
+      totalExpense = totalExpense + obj.total_value
+    }
+  })
+  return totalExpense
+}
+
+const getGrossProfitMargin = () => {
+  var gpm = 0;
+  data.data.forEach((obj)=>{
+    if(obj.account_type === "sales" && obj.value_type === "debit"){     
+      gpm = gpm + obj.total_value;
+    }
+  })
+  return gpm;
+}
+
+const getNetProfitMargin = () =>{
+  return ((getRevenue - getExpenses) / getRevenue)
+}
+
 
 function App() {
   const [revenue, setRevenue] = useState(0);
@@ -10,41 +45,12 @@ function App() {
   const [netProfitMargin, setNetProfitMargin] = useState(0);
   const [workingCapitalRatio, setWorkingCapitalRatio] = useState(0);
   
-  //console.log(data.data)
-    
-  const getRevenue = (data) => {
-      data.data.forEach((obj)=>{
-          if(obj.account_category === 'revenue'){
-              setRevenue(revenue + obj.total_value)
-          }
-      })
-  }
-
-  const getExpenses = (data) => {
-    data.data.forEach((obj)=>{
-      if(obj.account_category === 'expense'){
-          setExpenses(expenses + obj.total_value)
-      }
-    })
-  }
-
-  const getGrossProfitMargin = (data) => {
-    var sales = 0;
-    data.data.forEach((obj)=>{
-      if(obj.account_type === "sales" && obj.value_type === "debit"){     
-        sales = sales + obj.total_value;
-      }
-    })
-    setGrossProfitMargin(sales/revenue)
-  }
-
-  
-
 
   useEffect(() => {
-     getRevenue(data);
-     getExpenses(data);
-     getGrossProfitMargin(data);
+     setRevenue(getRevenue);
+     setExpenses(getExpenses);
+     setGrossProfitMargin(getGrossProfitMargin);
+     setNetProfitMargin(getNetProfitMargin);
   }, [])
 
   return (
@@ -52,8 +58,8 @@ function App() {
       <h1>9Spokes Challenge:</h1>
       <p>Revenue: {revenue}</p>
       <p>Expenses: {expenses}</p>
-      <p>Gross Profit Margin: {grossProfitMargin}</p>
-      <p>Net Profit Margin: {netProfitMargin}</p>
+      <p>Gross Profit Margin: {grossProfitMargin}%</p>
+      <p>Net Profit Margin: {netProfitMargin}%</p>
       <p>Working Capital Ratio: {workingCapitalRatio}</p>
 
     </div>
