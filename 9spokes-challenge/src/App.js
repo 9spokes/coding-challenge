ba9c07cbda358c05607cc887a0e5474d9b9e2ee5
Jordan D'Souza <jordandsouza352@gmail.com>
2021-07-29 17:49:07 +1200

    created revenue function

--- 9spokes-challenge/src/App.js
+++ 9spokes-challenge/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import data from './data.json'
+
 
 function App() {
   const [revenue, setRevenue] = useState(0);
@@ -7,6 +9,21 @@ function App() {
   const [grossProfitMargin, setGrossProfitMargin] = useState(0);
   const [netProfitMargin, setNetProfitMargin] = useState(0);
   const [workingCapitalRatio, setWorkingCapitalRatio] = useState(0);
+  
+  //console.log(data.data)
+    
+  const getRevenue = (data) => {
+      data.data.forEach((obj)=>{
+          if(obj.account_category === 'revenue'){
+              setRevenue(revenue + obj.total_value)
+          }
+      })
+  }
+
+  useEffect(() => {
+     getRevenue(data);
+
+  }, [])
 
   return (
     <div className="App">
