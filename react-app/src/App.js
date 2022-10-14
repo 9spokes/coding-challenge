ec9fc5b3652c3d5d6cc6d0d8a2ef765874db251f
SireeshaKare <kvpssireesha@gmail.com>
2022-10-14 19:30:19 +1300

    Work started

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+
+import data from './data/data.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const finanaceData = data.data.map((data) => {
+    return (
+      <Card key={data.account_identifier}>
+        <Card.Body>
+          <Card.Title>{data.account_name}</Card.Title>
+          <Card.Text>{}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
+
+  return finanaceData;
 }
 
 export default App;
